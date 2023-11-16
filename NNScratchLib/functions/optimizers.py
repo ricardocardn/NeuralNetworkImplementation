0acc54432fa7cb828c594e3ff88b06272d93f950
Ricardo Cárdenes <78762064+ricardocardn@users.noreply.github.com>
2023-11-16 16:09:48 +0100

    Adam optimizer implementation

--- NNScratchLib/functions/optimizers.py
+++ NNScratchLib/functions/optimizers.py
@@ -4,6 +4,7 @@ def gradient_descent(model, X, y, measure_function, epochs=100, learning_rate=0.
     X, X_val = X[:int(len(X)*0.8)], X[int(len(X)*0.8):]
     y, y_val = y[:int(len(y)*0.8)], y[int(len(y)*0.8):]
 
+    acc_list = []
     for epoch in range(epochs):
         for i in range(len(X)):
             dz, dW, dz2, dW2 = model.backpropagation(X[i], y[i])                
@@ -17,6 +18,9 @@ def gradient_descent(model, X, y, measure_function, epochs=100, learning_rate=0.
             Y_pred = [model.feedfoward(x) for x in X_val]
             acc = measure_function(y_val, Y_pred)
             print(f'epoch {epoch:3} - accuracy {acc:.5f}')
+            acc_list.append(acc)
+
+    return acc_list
 
 
 def momentum_gradient_descent(model, X, y, measure_function, epochs=100, learning_rate=0.01):
@@ -26,6 +30,7 @@ def momentum_gradient_descent(model, X, y, measure_function, epochs=100, learnin
     vW = [np.zeros(layer.weights.shape) for layer in model.layers]
     vb = [np.zeros(layer.bias.shape) for layer in model.layers]
 
+    acc_list = []
     for epoch in range(epochs):
         for i in range(len(X)):
             dz, dW, dz2, dW2 = model.backpropagation(X[i], y[i])                
@@ -45,3 +50,47 @@ def momentum_gradient_descent(model, X, y, measure_function, epochs=100, learnin
             Y_pred = [model.feedfoward(x) for x in X_val]
             acc = measure_function(y_val, Y_pred)
             print(f'epoch {epoch:3} - accuracy {acc:.5f}')
+            acc_list.append(acc)
+
+    return acc_list
+
+
+def Adam(model, X, y, measure_function, epochs=100, learning_rate=0.01):
+    X, X_val = X[:int(len(X)*0.8)], X[int(len(X)*0.8):]
+    y, y_val = y[:int(len(y)*0.8)], y[int(len(y)*0.8):]
+
+    vW = [np.zeros(layer.weights.shape) for layer in model.layers]
+    vb = [np.zeros(layer.bias.shape) for layer in model.layers]
+
+    sW = [np.zeros(layer.weights.shape) for layer in model.layers]
+    sb = [np.zeros(layer.bias.shape) for layer in model.layers]
+
+    acc_list = []
+    for epoch in range(epochs):
+        for i in range(len(X)):
+            dz, dW, dz2, dW2 = model.backpropagation(X[i], y[i])
+            params = [dW2, dz2, dW, dz]
+
+            for layer in range(len(model.layers)):
+                vW[layer] = 0.9*vW[layer] + (1-0.9)*params[2*layer]
+                vb[layer] = 0.9*vb[layer] + (1-0.9)*params[2*layer+1]
+
+                sW[layer] = 0.999*sW[layer] + (1-0.999)*params[2*layer]**2
+                sb[layer] = 0.999*sb[layer] + (1-0.999)*params[2*layer+1]**2
+
+                vW_corrected = vW[layer]/(1-0.9**(epoch+1))
+                vb_corrected = vb[layer]/(1-0.9**(epoch+1))
+
+                sW_corrected = sW[layer]/(1-0.999**(epoch+1))
+                sb_corrected = sb[layer]/(1-0.999**(epoch+1))
+
+                model.layers[layer].weights -= learning_rate*vW_corrected/(np.sqrt(sW_corrected) + 1e-8)
+                model.layers[layer].bias -= learning_rate*vb_corrected/(np.sqrt(sb_corrected) + 1e-8)
+
+        if epoch % 10 == 0:
+            Y_pred = [model.feedfoward(x) for x in X_val]
+            acc = measure_function(y_val, Y_pred)
+            print(f'epoch {epoch:3} - accuracy {acc:.5f}')
+            acc_list.append(acc)
+
+    return acc_list
